cb15f311f4eb3afac479d08dbea90fe84e461f4e
Aiden G <orca950@gmail.com>
2017-12-01 21:52:44 -0600

    added to github

---
+++ CaseyLP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Linq;
+
+public class Program
+{
+    private CommandService commands;
+    private DiscordSocketClient client;
+    private IServiceProvider services;
+    string mainDir;
+    string roleFiles;
+    string userFiles;
+
+    public Program()
+    {
+        mainDir = "C:/srab";
+        roleFiles = Path.Combine(mainDir, "usersroles");
+        userFiles = Path.Combine(mainDir, "users");
+    }
+
+    static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+    public async Task Start()
+    {
+        client = new DiscordSocketClient();
+        commands = new CommandService();
+        string token = ""; //bot token here
+        client.UserJoined += AnnounceJoinedUser; //Check if userjoined
+
+        services = new ServiceCollection()
+                .BuildServiceProvider();
+
+ 
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    public async Task AnnounceJoinedUser(SocketGuildUser user) //welcomes New Users
+    {
+        SocketGuild guid = client.GetGuild(/*guild id*/);
+        SocketTextChannel channel = guid.GetChannel(/*channel id*/) as SocketTextChannel;
+        await channel.SendMessageAsync("Welcome " + user.Mention);
+    }
+}
